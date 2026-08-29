98f4af0d4834cfe1e8eb1341074e996fcc6bcada
AlexJustine0802 <alexanderjustine0802@gmail.com>
2026-08-29 20:19:58 +0700

    updates

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -15,7 +15,7 @@ export default async function SiteLayout({
 
   return (
     <div className="site-shell flex min-h-screen flex-col">
-      <Navbar homeHref={session ? "/account" : "/"} />
+      <Navbar homeHref={session ? "/pfcspace.com" : "/"} />
       {/* The wrapper sits below the navbar on purpose: animating a transform
           on an ancestor would break the navbar's sticky positioning. */}
       <main className="min-w-0 flex-1">
